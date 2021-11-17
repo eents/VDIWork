7f7b659e61db4cfd046ada9c07a2193e0467e959
eents <60370827+eents@users.noreply.github.com>
2021-11-16 18:02:36 -0600

    2

---
+++ WPFUserControl2/WPFDogLibrary/IMammal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace WPFDogLibrary
+{
+    public interface IMammal
+    {
+        int Age { get; set; }
+        int Weight { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ WPFUserControl2/WPFDogLibrary/Mammal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace WPFDogLibrary
+{
+    public abstract class Mammal : IMammal
+    {
+        public int Age
+        {
+            get;
+            set;
+        }
+        
+        public int Weight
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
