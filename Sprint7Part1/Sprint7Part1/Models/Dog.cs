05214aecbd3f3a09cc3f590c938b2a9697a5e36e
eents <60370827+eents@users.noreply.github.com>
2021-11-17 16:25:32 -0600

    3

--- Sprint7Part1/Sprint7Part1/Models/Dog.cs
+++ Sprint7Part1/Sprint7Part1/Models/Dog.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Sprint7Part1.Models
 {
-    class Dog
+    class Dog : INotifyPropertyChanged
     {
         public int Age { get; set; }
 
@@ -16,6 +18,8 @@ namespace Sprint7Part1.Models
             set
             {
                 weight = value;
+                OnPropertyChanged();
+                OnPropertyChanged("AboutString");
             }
         }
 
@@ -40,6 +44,20 @@ namespace Sprint7Part1.Models
             this.Weight = 60;
             this.Age = 8;
             this.Name = "Daisy";
+        }
+
+        public string AboutString { get => About(); }
+
+        public string About()
+        {
+            return $"Hello my name is {Name}. I am {Age} years old and I weigh {Weight} pounds.";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
