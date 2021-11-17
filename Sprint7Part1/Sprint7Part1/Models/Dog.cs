5dc671523bd9d5e8d57fffc24d8d8b9c8198d344
eents <60370827+eents@users.noreply.github.com>
2021-11-17 15:17:44 -0600

    Start assignment 2

---
+++ Sprint7Part1/Sprint7Part1/Models/Dog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint7Part1.Models
+{
+    class Dog
+    {
+        public int Age { get; set; }
+
+        protected int weight;
+
+        public int Weight
+        {
+            get => weight;
+            set
+            {
+                weight = value;
+            }
+        }
+
+        protected string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+            }
+        }
+
+        public void HappyBirthday()
+        {
+            this.Age++;
+        }
+
+        public Dog()
+        {
+            this.Weight = 60;
+            this.Age = 8;
+            this.Name = "Daisy";
+        }
+    }
+}
